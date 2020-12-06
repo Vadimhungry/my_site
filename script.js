fa14068394cf7ed215bca17484b0186935ab2421
Vadimhungry <52575901+Vadimhungry@users.noreply.github.com>
2020-12-06 20:58:09 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-let page = document.querySelector('.page');
-let themeButon = document.querySelector('.theme-button');
-themeButon.onclick = function () {
-	page.classList.toggle('dark-theme');
-};
